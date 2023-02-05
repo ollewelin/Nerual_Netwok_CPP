43e030555e363da1526387d65340c72aa0c9a88d
Olle Welin <olle_welin@hotmail.com>
2023-02-05 13:18:37 +0400

    Add files via upload

--- simple_nn.hpp
+++ simple_nn.hpp
@@ -22,8 +22,18 @@ public:
     double loss;
     double learning_rate;
     double momentum;
+    int use_softmax;
     vector<double> input_layer;
     vector<double> output_layer;
+    vector<double> target_layer;
+
+
+    void randomize_weights(double);//the input argument must be in range 0..1 but to somthing low for example 0.01
+    void load_weights(string);//load weights with file name argument 
+    void save_weights(string);//save weights with file name argument 
+    void forward_pass(void);
+   // void only_loss_calculation(void);
+    void backpropagtion_and_update(void);//If batchmode update only when batch end
 
     
     simple_nn();
