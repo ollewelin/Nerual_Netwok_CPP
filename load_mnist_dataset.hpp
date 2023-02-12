22331cc8d52e0161a94fc3a19fde7d7a209aeb5a
Olle Welin <olle_welin@hotmail.com>
2023-02-12 17:11:16 +0100

    Add files via upload

--- load_mnist_dataset.hpp
+++ load_mnist_dataset.hpp
@@ -9,6 +9,11 @@ class load_mnist_dataset
 {
 private:
   char * memblock;
+  void download_mnist(void);
+  string filename_tr_data;
+  string filename_tr_lable;
+  string filename_ver_data;
+  string filename_ver_lable;
 
 public:
     vector<vector<double>> load_input_data(vector<vector<double>> input_dat, int verify_mode);
