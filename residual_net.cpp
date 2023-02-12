1cacee7405729974e0247e99d65082fb703ede79
Olle Welin <olle_welin@hotmail.com>
2023-02-12 14:52:02 +0100

    Add files via upload

--- residual_net.cpp
+++ residual_net.cpp
@@ -2,10 +2,9 @@
 #include <iostream>
 #include <stdio.h>
 
+//#include "load_mnist_dataset.hpp"
+
 #include "fc_m_resnet.hpp"
-
-#include "load_mnist_dataset.hpp"
-
 #include <vector>
 #include <time.h>
 
@@ -20,18 +19,157 @@ using namespace std;
 using namespace std;
 #include <cstdlib>
 
+char MNIST_filename[100];
+/// Input data from
+/// t10k-images-idx3-ubyte
+/// t10k-labels-idx1-ubyte
+/// train-images-idx3-ubyte
+/// train-labels-idx1-ubyte
+/// http://yann.lecun.com/exdb/mnist/
+int use_MNIST_verify_set = 0;
+const int MNIST_height = 28;
+const int MNIST_width = 28;
+int MNIST_nr_of_img_p_batch = 60000;
+/// const int Const_nr_pic = 60000;
+const int MNIST_pix_size = MNIST_height * MNIST_width;
+const int MNIST_RGB_pixels = MNIST_pix_size;
+/// char data_10k_MNIST[10000][MNIST_pix_size];
+/// char data_60k_MNIST[60000][MNIST_pix_size];
+
+const int MNIST_header_offset = 16;
+const int MNIST_lable_offset = 8;
+/*
+TEST SET LABEL FILE (t10k-labels-idx1-ubyte):
+[offset] [type]          [value]          [description]
+0000     32 bit integer  0x00000801(2049) magic number (MSB first)
+0004     32 bit integer  10000            number of items
+0008     unsigned byte   ??               label
+0009     unsigned byte   ??               label
+........
+xxxx     unsigned byte   ??               label
+The labels values are 0 to 9.
+TEST SET IMAGE FILE (t10k-images-idx3-ubyte):
+[offset] [type]          [value]          [description]
+0000     32 bit integer  0x00000803(2051) magic number
+0004     32 bit integer  10000            number of images
+0008     32 bit integer  28               number of rows
+0012     32 bit integer  28               number of columns
+0016     unsigned byte   ??               pixel
+0017     unsigned byte   ??               pixel
+........
+xxxx     unsigned byte   ??               pixel
+Pixels are organized row-wise. Pixel values are 0 to 255. 0 means background (white), 255 means foreground (black).
+*/
+
+int get_MNIST_lable_file_size(void);
+int get_MNIST_file_size(void);
+
 vector<int> fisher_yates_shuffle(vector<int> table);
 
 int main()
 {
- 
-
 
   cout << "General Neural Network Residual net test Beta version under work..." << endl;
   cout << "3 stackaed nn blocks with residual connections " << endl;
   srand(time(NULL));
   char answer;
   char answer_character;
+  //=====get MNIST ==
+  int MNIST_file_size = 0;
+  /// Read database train-images-idx3-ubyte
+  MNIST_file_size = get_MNIST_file_size();
+  cout << "MNIST_file_size = " << (int)MNIST_file_size << endl;
+
+  // read_10k_MNIST();
+  char *MNIST_data;
+  MNIST_data = new char[MNIST_file_size];
+  FILE *fp;
+  char c_data = 0;
+  if (use_MNIST_verify_set == 0)
+  {
+    fp = fopen("train-images-idx3-ubyte", "rb");
+  }
+  else
+  {
+    fp = fopen("t10k-images-idx3-ubyte", "rb");
+  }
+  if (fp == NULL)
+  {
+    perror("Error in opening train-images-idx3-ubyte file");
+    return (-1);
+  }
+  int MN_index = 0;
+  for (int i = 0; i < MNIST_file_size; i++)
+  {
+    c_data = fgetc(fp);
+   // cout << "c_data = " << (int)c_data << endl;
+    if (feof(fp))
+    {
+      break;
+    }
+    // printf("c_data %d\n", c_data);
+    MNIST_data[MN_index] = c_data;
+
+    if ((MNIST_header_offset - 1) < i)
+    {
+      MN_index++;
+    }
+  }
+
+  //while(1){};
+
+  fclose(fp);
+  printf("train.. or t10k.. ..-images-idx3-ubyte file is successfully loaded in to MNIST_data[MN_index] memory\n");
+  /// Read lable
+  /// Read train-labels-idx1-ubyte
+  MNIST_file_size = get_MNIST_lable_file_size();
+  // read_10k_MNIST();
+  char *MNIST_lable;
+  MNIST_lable = new char[MNIST_file_size];
+  // FILE *fp;
+  c_data = 0;
+  if (use_MNIST_verify_set == 0)
+  {
+    fp = fopen("train-labels-idx1-ubyte", "rb");
+  }
+  else
+  {
+    fp = fopen("t10k-labels-idx1-ubyte", "rb");
+  }
+
+  if (fp == NULL)
+  {
+    if (use_MNIST_verify_set == 0)
+    {
+      perror("Error in opening train-labels-idx1-ubyte file");
+    }
+    else
+    {
+      perror("Error in opening t10k-labels-idx1-ubyte file");
+    }
+
+    return (-1);
+  }
+  MN_index = 0;
+  for (int i = 0; i < MNIST_file_size; i++)
+  {
+
+    c_data = fgetc(fp);
+    if (feof(fp))
+    {
+      break;
+    }
+    // printf("c_data %d\n", c_data);
+    MNIST_lable[MN_index] = c_data;
+    if ((MNIST_lable_offset - 1) < i)
+    {
+      MN_index++;
+    }
+  }
+  fclose(fp);
+  printf("train... or t10k...  ...-labels-idx1-ubyte file is successfully loaded in to MNIST_lable[MN_index] memory\n");
+
+  //==========
 
   //=========== Test Neural Network size settings ==============
   fc_m_resnet fc_nn_top_block;
@@ -64,17 +202,8 @@ int main()
   fc_nn_end_block.activation_function_mode = 0;
   fc_nn_end_block.use_skip_connect_mode = 0;//1 for residual network architetcture
   fc_nn_end_block.use_dopouts = 0;
-
-  load_mnist_dataset l_mnist_data;
-  vector<vector<double>> training_target_data;
-  vector<vector<double>> training_input_data;
-  vector<vector<double>> verify_target_data;
-  vector<vector<double>> verify_input_data;
-  int data_size_one_sample = l_mnist_data.get_one_sample_data_size();
-  int training_dataset_size = l_mnist_data.get_training_data_set_size();
  
-
-  const int top_inp_nodes = data_size_one_sample;
+  const int top_inp_nodes = MNIST_pix_size;
   const int top_out_nodes = 30;
   const int mid_out_nodes = 30;
   const int end_out_nodes = 10;
@@ -88,35 +217,6 @@ int main()
   const int end_hid_nodes_L1 = 15;
   //const int end_hid_nodes_L2 = 15;
   //const int hid_nodes_L3 = 7;
-
-  vector<double> dummy_one_target_data_point;
-  vector<double> dummy_one_training_data_point;
-  for (int i = 0; i < end_out_nodes; i++)
-  {
-    dummy_one_target_data_point.push_back(0.0);
-  }
-  for (int i = 0; i < top_inp_nodes; i++)
-  {
-    dummy_one_training_data_point.push_back(0.0);
-  }
-  for (int i = 0; i < training_dataset_size; i++)
-  {
-    training_target_data.push_back(dummy_one_target_data_point);
-    training_input_data.push_back(dummy_one_training_data_point);
-  }
-  for (int i = 0; i < l_mnist_data.get_verify_data_set_size(); i++)
-  {
-    verify_target_data.push_back(dummy_one_target_data_point);
-    verify_input_data.push_back(dummy_one_training_data_point);
-  }
-
-  training_input_data = l_mnist_data.load_input_data(training_input_data, 0);//last argument 0 = training, 1 = verify
-  training_target_data = l_mnist_data.load_lable_data(training_target_data, 0);//last argument 0 = training, 1 = verify
-  verify_input_data = l_mnist_data.load_input_data(verify_input_data, 1);//last argument 0 = training, 1 = verify
-  verify_target_data = l_mnist_data.load_lable_data(verify_target_data, 1);//last argument 0 = training, 1 = verify
-  
-  l_mnist_data.~load_mnist_dataset();
-
   for (int i = 0; i < top_inp_nodes; i++)
   {
     fc_nn_top_block.input_layer.push_back(0.0);
@@ -196,12 +296,17 @@ int main()
   const int training_epocs = 10000; // One epocs go through the hole data set once
   const int save_after_epcs = 10;
   int save_epoc_counter = 0;
+  const int training_dataset_size = MNIST_nr_of_img_p_batch;
   const int verify_dataset_size = 100;
   const int verify_after_x_nr_epocs = 10;
   int verify_after_epc_cnt = 0;
   double best_training_loss = 1000000000;
   double best_verify_loss = best_training_loss;
   const double stop_training_when_verify_rise_propotion = 0.02;
+  vector<vector<double>> training_target_data;
+  vector<vector<double>> training_input_data;
+  vector<vector<double>> verify_target_data;
+  vector<vector<double>> verify_input_data;
   vector<int> training_order_list;
   vector<int> verify_order_list;
   for (int i = 0; i < training_dataset_size; i++)
@@ -215,18 +320,77 @@ int main()
   verify_order_list = fisher_yates_shuffle(verify_order_list);
   training_order_list = fisher_yates_shuffle(training_order_list);
 
+  vector<double> dummy_one_target_data_point;
+  vector<double> dummy_one_training_data_point;
+  for (int i = 0; i < end_out_nodes; i++)
+  {
+    dummy_one_target_data_point.push_back(0.0);
+  }
+  for (int i = 0; i < top_inp_nodes; i++)
+  {
+    dummy_one_training_data_point.push_back(0.0);
+  }
+  for (int i = 0; i < training_dataset_size; i++)
+  {
+    training_target_data.push_back(dummy_one_target_data_point);
+    training_input_data.push_back(dummy_one_training_data_point);
+  }
+  for (int i = 0; i < verify_dataset_size; i++)
+  {
+    verify_target_data.push_back(dummy_one_target_data_point);
+    verify_input_data.push_back(dummy_one_training_data_point);
+  }
 
   int MNIST_nr = 0;
-  srand (static_cast <unsigned> (time(NULL)));//Seed the randomizer
-
+    srand (static_cast <unsigned> (time(NULL)));//Seed the randomizer
+  // Start traning
   int do_verify_if_best_trained = 0;
   int stop_training = 0;
+  int  target_lable = 0;
+  char mnist_d = 0;
+  double mnist_f = 0.0;
+  unsigned char mnist_uchar = 0.0;
+  for (int i = 0; i < training_dataset_size; i++)
+  {
+    for(int j=0;j<MNIST_pix_size;j++)
+    {
+    mnist_d = MNIST_data[(MNIST_pix_size * i) + j];
+  //  cout << " mnist_d = " << (int)mnist_d  << endl;
+    mnist_uchar = (~(-mnist_d) + 1);
+  //  cout << " mnist_uchar = " <<  (int)mnist_uchar  << endl;
+    mnist_f = ((double)mnist_uchar)/256.0;
+
+  //  cout << "mnist_f = " << mnist_f  << endl;
+    training_input_data[i][j] = mnist_f;       // 0..1
+    }
+    target_lable = ((int) MNIST_lable[i]);
+    for(int j=0;j<end_out_nodes;j++)
+    {
+      float targ_dig = 0.0;
+      if(target_lable == j)
+      {
+        targ_dig = 1.0;
+      }
+      training_target_data[i][j] = targ_dig;
+    }
+  }
+  MNIST_nr = (int)(rand() % MNIST_nr_of_img_p_batch);
   
+  for (int n = 0; n < MNIST_pix_size; n++)
+  {
+    mnist_d = MNIST_data[(MNIST_pix_size * MNIST_nr) + n];
+    mnist_uchar = (~(-mnist_d) + 1);
+    mnist_f = ((double)mnist_uchar)/256.0;
+  }
+
+
+  target_lable = ((int) MNIST_lable[MNIST_nr]);
+  cout << "target_lable = " << target_lable  << endl;
 
 
 double test_one_i_delta = 0.0;
 double pre_test_one_i_delta = 0.0;
-  // Start traning
+
 //=================
 
   for (int epc = 0; epc < training_epocs; epc++)
@@ -239,6 +403,8 @@ double pre_test_one_i_delta = 0.0;
     cout << "input node --- [0] = " << fc_nn_end_block.input_layer[0] << endl;
 
     //============ Traning ==================
+
+    MNIST_nr = (int)(rand() % MNIST_nr_of_img_p_batch);
 
     // verify_order_list = fisher_yates_shuffle(verify_order_list);
     training_order_list = fisher_yates_shuffle(training_order_list);
@@ -366,4 +532,168 @@ vector<int> fisher_yates_shuffle(vector<int> table)
     table[j] = temp;
   }
   return table;
+}
+
+void check_file_exist(char *filename)
+{
+  FILE *fp2 = fopen(filename, "rb");
+  if (fp2 == NULL)
+  {
+    cout << "Error " << filename << " file doesn't exist" << endl;
+    ;
+    cout << "Maybee download failure try restart program later download again" << endl;
+    ;
+    printf("Exit program\n");
+    exit(0);
+  }
+  else
+  {
+    cout << "OK " << filename << " file does exist" << endl;
+    ;
+  }
+  fclose(fp2);
+}
+int get_MNIST_file_size(void)
+{
+  char answer_character;
+  int file_size = 0;
+  FILE *fp2;
+  if (use_MNIST_verify_set == 0)
+  {
+    fp2 = fopen("train-images-idx3-ubyte", "rb");
+  }
+  else
+  {
+    fp2 = fopen("t10k-images-idx3-ubyte", "rb");
+  }
+  if (fp2 == NULL)
+  {
+    if (use_MNIST_verify_set == 0)
+    {
+      puts("Error!  while opening file train-images-idx3-ubyte");
+    }
+    else
+    {
+      puts("Error! while opening file t10k-images-idx3-ubyte");
+    }
+    printf("Suggestions, download and extraxt dataset into program folder\n");
+    printf("Do you want to download MNIST data set from web Y/N ?\n");
+    printf("From web sites:\n");
+    printf("http://yann.lecun.com/exdb/mnist/train-images-idx3-ubyte.gz\n");
+    printf("http://yann.lecun.com/exdb/mnist/train-labels-idx1-ubyte.gz\n");
+    printf("http://yann.lecun.com/exdb/mnist/t10k-images-idx3-ubyte.gz\n");
+    printf("http://yann.lecun.com/exdb/mnist/t10k-labels-idx1-ubyte.gz\n");
+    answer_character = getchar();
+    // answer_character = getchar();
+    if (answer_character == 'Y' || answer_character == 'y')
+    {
+      printf("remove old MNIST dataset .gz files\n");
+      system("rm train-images-idx3-ubyte.gz");
+      system("rm train-labels-idx1-ubyte.gz");
+      system("rm t10k-images-idx3-ubyte.gz");
+      system("rm t10k-labels-idx1-ubyte.gz");
+      printf("Start download MNIST dataset .gz files\n");
+      system("wget http://yann.lecun.com/exdb/mnist/train-images-idx3-ubyte.gz");
+      system("wget http://yann.lecun.com/exdb/mnist/train-labels-idx1-ubyte.gz");
+      system("wget http://yann.lecun.com/exdb/mnist/t10k-images-idx3-ubyte.gz");
+      system("wget http://yann.lecun.com/exdb/mnist/t10k-labels-idx1-ubyte.gz");
+
+      /* try to open file to read */
+      sprintf(MNIST_filename, "train-images-idx3-ubyte.gz");
+      check_file_exist(MNIST_filename);
+      sprintf(MNIST_filename, "train-labels-idx1-ubyte.gz");
+      check_file_exist(MNIST_filename);
+      sprintf(MNIST_filename, "t10k-images-idx3-ubyte.gz");
+      check_file_exist(MNIST_filename);
+      sprintf(MNIST_filename, "t10k-labels-idx1-ubyte.gz");
+      check_file_exist(MNIST_filename);
+
+      printf("Do you want to unzip this files with gzip Y/N\n");
+      answer_character = getchar();
+      answer_character = getchar();
+
+      if (answer_character == 'Y' || answer_character == 'y')
+      {
+        printf("Unzip MNIST datasets\n");
+        system("gzip -d train-images-idx3-ubyte.gz");
+        system("gzip -d train-labels-idx1-ubyte.gz");
+        system("gzip -d t10k-images-idx3-ubyte.gz");
+        system("gzip -d t10k-labels-idx1-ubyte.gz");
+        printf("...\n");
+      }
+      else
+      {
+        printf("You now may need to unzip all mnist....gz files\n");
+        printf("Try command:\n");
+        printf("$ gzip -d train-images-idx3-ubyte.gz\n");
+        printf("Exit program\n");
+        exit(0);
+      }
+    }
+    else
+    {
+      printf("OK you may need to download MNIST dataset\n");
+      printf("Exit program\n");
+      exit(0);
+    }
+
+    if (use_MNIST_verify_set == 0)
+    {
+      fp2 = fopen("train-images-idx3-ubyte", "rb");
+    }
+    else
+    {
+      fp2 = fopen("t10k-images-idx3-ubyte", "rb");
+    }
+    if (fp2 == NULL)
+    {
+      printf("exit program MNIST dataset failure to open\n");
+      exit(0);
+    }
+    else
+    {
+      printf("OK! MNIST dataset daownloade and extracted\n");
+    }
+  }
+
+  fseek(fp2, 0L, SEEK_END);
+  file_size = ftell(fp2);
+  printf("file_size %d\n", file_size);
+  rewind(fp2);
+  fclose(fp2);
+  return file_size;
+}
+
+int get_MNIST_lable_file_size(void)
+{
+  int file_size = 0;
+  FILE *fp2;
+  if (use_MNIST_verify_set == 0)
+  {
+    fp2 = fopen("train-labels-idx1-ubyte", "rb");
+  }
+  else
+  {
+    fp2 = fopen("t10k-labels-idx1-ubyte", "rb");
+  }
+
+  if (fp2 == NULL)
+  {
+    if (use_MNIST_verify_set == 0)
+    {
+      puts("Error while opening file train-labels-idx1-ubyte");
+    }
+    else
+    {
+      puts("Error while opening file t10k-labels-idx1-ubyte");
+    }
+    exit(0);
+  }
+
+  fseek(fp2, 0L, SEEK_END);
+  file_size = ftell(fp2);
+  printf("file_size %d\n", file_size);
+  rewind(fp2);
+  fclose(fp2);
+  return file_size;
 }
