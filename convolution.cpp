b9f21cfa6737a61e0742a26221338c4e18a3e21f
Olle Welin <olle_welin@hotmail.com>
2023-03-04 20:10:19 +0100

    Add files via upload

--- convolution.cpp
+++ convolution.cpp
@@ -443,7 +443,7 @@ void convolution::xy_start_stop_kernel(int slide_val)
         start_ret = start_ret + (slide_val - output_side_size * stride);
     }
     stop_ret = slide_val;
-    if (stop_ret < kernel_size)
+    if (stop_ret > kernel_size)
     {
         stop_ret = kernel_size;
     }
