8a1889486fd4ffd3038ac940eb002f6391ee07ce
Olle Welin <olle_welin@hotmail.com>
2023-03-05 08:41:03 +0100

    Add files via upload

--- conv_transpose/main.c
+++ conv_transpose/main.c
@@ -3,34 +3,32 @@
 
 using namespace std;
 
-// Function to perform transpose convolution operation
-vector<vector<vector<double>>> transposeConvolution(vector<vector<vector<double>>> input, 
-                                                    vector<vector<vector<double>>> kernel,
-                                                    int stride) {
+// Transpose convolution operation
+vector<vector<vector<double>>> transposeConvolution(
+    vector<vector<vector<double>>> input,
+    vector<vector<vector<vector<double>>>> kernel,
+    int stride)
+{
     int input_channels = input.size();
     int output_channels = kernel[0].size();
     int kernel_size = kernel[0][0].size();
-    int input_size = input[0].size();
-    int output_size = (input_size - 1) * stride + kernel_size;
+    int output_size = (input[0][0].size() - 1) * stride + kernel_size;
 
-    // Initialize output vector
+    // Initialize output tensor with zeros
     vector<vector<vector<double>>> output(output_channels, 
                                            vector<vector<double>>(output_size, 
                                                                    vector<double>(output_size)));
 
-    // Iterate over output channels
+    // Perform transpose convolution operation
     for (int oc = 0; oc < output_channels; oc++) {
-        // Iterate over input channels
         for (int ic = 0; ic < input_channels; ic++) {
-            // Iterate over output spatial position
-            for (int xo = 0; xo < output_size; xo++) {
-                for (int yo = 0; yo < output_size; yo++) {
-                    // Iterate over kernel spatial position
+            for (int xo = 0; xo < output_size; xo += stride) {
+                for (int yo = 0; yo < output_size; yo += stride) {
                     for (int xk = 0; xk < kernel_size; xk++) {
                         for (int yk = 0; yk < kernel_size; yk++) {
-                            int xi = xo + xk - stride;
-                            int yi = yo + yk - stride;
-                            if (xi >= 0 && xi < input_size && yi >= 0 && yi < input_size) {
+                            int xi = xo + xk - kernel_size + 1;
+                            int yi = yo + yk - kernel_size + 1;
+                            if (xi >= 0 && xi < input[ic][0].size() && yi >= 0 && yi < input[ic].size()) {
                                 output[oc][xo][yo] += input[ic][xi][yi] * kernel[ic][oc][xk][yk];
                             }
                         }
@@ -39,37 +37,38 @@ vector<vector<vector<double>>> transposeConvolution(vector<vector<vector<double>
             }
         }
     }
-    
+
     return output;
 }
 
-int main() {
-    // Set arbitrary input, kernel and output sizes
+int main()
+{
+    // Set up input tensor
     int input_channels = 3;
-    int output_channels = 8;
-    int kernel_size = 5;
-    int input_size = 7;
-
-    // Initialize input vector
-    vector<vector<vector<double>>> input(input_channels, 
+    int input_size = 10;
+    vector<vector<vector<double>>> input(input_channels,
                                           vector<vector<double>>(input_size, 
                                                                   vector<double>(input_size)));
-    // Initialize kernel vector
-    vector<vector<vector<vector<double>>>> kernel(input_channels, 
-                                                   vector<vector<vector<double>>>(output_channels, 
-                                                                                   vector<vector<double>>(kernel_size, 
-                                                                                                           vector<double>(kernel_size))));
-    // Initialize input and kernel with arbitrary values
-    for (int i = 0; i < input_channels; i++) {
-        for (int j = 0; j < input_size; j++) {
-            for (int k = 0; k < input_size; k++) {
-                input[i][j][k] = i * j * k;
+    for (int ic = 0; ic < input_channels; ic++) {
+        for (int xi = 0; xi < input_size; xi++) {
+            for (int yi = 0; yi < input_size; yi++) {
+                input[ic][xi][yi] = xi * yi;
             }
         }
-        for (int o = 0; o < output_channels; o++) {
-            for (int x = 0; x < kernel_size; x++) {
-                for (int y = 0; y < kernel_size; y++) {
-                    kernel[i][o][x][y] = (i + 1) * (o + 1) * (x + 1) * (y + 1);
+    }
+
+    // Set up kernel tensor
+    int output_channels = 8;
+    int kernel_size = 5;
+    vector<vector<vector<vector<double>>>> kernel(input_channels,
+                                                    vector<vector<vector<double>>>(output_channels,
+                                                                                    vector<vector<double>>(kernel_size,
+                                                                                                            vector<double>(kernel_size))));
+    for (int ic = 0; ic < input_channels; ic++) {
+        for (int oc = 0; oc < output_channels; oc++) {
+            for (int xk = 0; xk < kernel_size; xk++) {
+                for (int yk = 0; yk < kernel_size; yk++) {
+                    kernel[ic][oc][xk][yk] = xk * yk;
                 }
             }
         }
@@ -78,7 +77,7 @@ int main() {
     // Perform transpose convolution operation with stride = 2
     int stride = 2;
     vector<vector<vector<double>>> output = transposeConvolution(input, kernel, stride);
-
+/*
    // Print output
     for (int i = 0; i < output_channels; i++) {
         cout << "Output Channel " << i + 1 << ":" << endl;
@@ -90,6 +89,6 @@ int main() {
         }
         cout << endl;
     }
-
+*/
     return 0;
 }
